5785f2e86456a0635eea1ac2c948dca46891ad9f
Aken Mehta <mehta.aken@gmail.com>
2017-08-10 09:12:56 -0400

    added main section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,24 @@
-console.log('connected');
+var app = {};
+app.windowSize = $(window).width();
 
+app.removeClass = function() {
+	if(app.windowSize <= 1007) {
+		$('.last-child').removeClass('invisible');
+	}
+};
+
+app.windowResize = function() {
+	$(window).resize(function() {
+  		app.windowSize = $(window).width();
+  		app.removeClass();
+	});
+};
+
+app.init = function() {
+	app.windowResize();
+	app.removeClass();
+};
+
+$(function() {
+	app.init();
+});
