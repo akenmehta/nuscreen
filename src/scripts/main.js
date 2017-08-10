b21a8ce5d5f952ee9d2dfbc3b9676eea6cf613e0
Aken Mehta <mehta.aken@gmail.com>
2017-08-10 12:43:15 -0400

    added image grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,14 @@
 var app = {};
 app.windowSize = $(window).width();
 
-app.removeClass = function() {
+// app.hover = function() {
+// 	$('.image-grid-child').hover(function() {
+// 		console.log(this);
+// 		$('div.image__flex-container').addClass('teal-overlay');
+// 	});
+// }
+
+app.removeClassInvisible = function() {
 	if(app.windowSize <= 1007) {
 		$('.last-child').removeClass('invisible');
 	}
@@ -10,13 +17,14 @@ app.removeClass = function() {
 app.windowResize = function() {
 	$(window).resize(function() {
   		app.windowSize = $(window).width();
-  		app.removeClass();
+  		app.removeClassInvisible();
 	});
 };
 
 app.init = function() {
 	app.windowResize();
-	app.removeClass();
+	app.removeClassInvisible();
+	// app.hover();
 };
 
 $(function() {
