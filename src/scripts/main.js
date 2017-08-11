641e692e1d1241a5b8390e52e2ebe9417973f2b2
Aken Mehta <mehta.aken@gmail.com>
2017-08-11 09:01:20 -0400

    added mobile toggle menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 var app = {};
 app.windowSize = $(window).width();
 
+app.toogleMenu = function() {
+	$('.nav-btn').on('click', function() {
+		$('.menu-items').toggleClass('invisible');
+	})
+}
+
+app.showNavBtn = function() {
+	if(app.windowSize <= 723) {
+		$('.nav-btn').removeClass('invisible');
+		$('.menu-items').addClass('invisible');
+	}
+}
+
 app.removeClassInvisible = function() {
 	if(app.windowSize <= 1007 && app.windowSize >= 623) {
 		$('.last-child').removeClass('invisible');
 	}
-
 	else if(app.windowSize <= 623) {
 		$('.last-child').addClass('invisible');
 	}
@@ -15,6 +27,7 @@ app.windowResize = function() {
 	$(window).resize(function() {
   		app.windowSize = $(window).width();
   		app.removeClassInvisible();
+  		app.showNavBtn();
   		console.log(app.windowSize);
 	});
 };
@@ -22,7 +35,8 @@ app.windowResize = function() {
 app.init = function() {
 	app.windowResize();
 	app.removeClassInvisible();
-	
+	app.showNavBtn();
+	app.toogleMenu();
 };
 
 $(function() {
